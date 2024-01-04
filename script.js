758c583b50564d189f7c1052d357a72ef11c8720
Diogo S. Florêncio <diogosflorencio@gmail.com>
2024-01-04 14:48:01 -0300

    fix: acrescentando itens

--- script.js
+++
@@ -1,9 +0,0 @@
-const inputDoUsuario = document.getElementById("inputDoUsario");
-const botaoAdicionar = document.getElementById('botaoAdicionar');
-
-const item1 = document.getElementById("item-1");
-
-botaoAdicionar.addEventListener("click", () => {
-    item1.innerHTML = inputDoUsuario.value;
-    inputDoUsuario.value = ""
-});
